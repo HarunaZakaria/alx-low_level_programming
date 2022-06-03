126bfa0d8c19aed43bfcafaad7d0b28cfe060e81
harunazakaria <Harunzy55@gmail.com>
2022-06-02 17:22:37 -0700

    juh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - prints a given phrase
- * Return: 0 if existed properly, non-zero otherwise
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+int main(void)
 {
-	puts("\"programming is likebuilding a multilingual puzzle");
-	return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	retunr(0);
 }
