9c757543791a02979b18b97c0951c875a447dcdb
harunazakaria <Harunzy55@gmail.com>
2022-06-02 17:04:21 -0700

    tbf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if existed properly, non-zero otherwise
+ */
+{
+	puts("\"programming is likebuilding a multilingual puzzle");
+	return (0);
+}
