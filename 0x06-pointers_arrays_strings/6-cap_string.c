223d83313969540dc177014db02e2cbda1eb4c9f
harunazakaria <Harunzy55@gmail.com>
2022-06-17 13:34:40 -0700

    string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,9 @@
 
 char *cap_string(char *str)
 {
-	int i; c;
+	int i, c;
 	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 	char nots[] = ",;.!?(){}\n\t\" ";
 
 	for (i = 0, trigger = 0; str[i] != '\0'; i++)
@@ -21,7 +22,7 @@ char *cap_string(char *str)
 			if (nots[c] == str[i])
 				trigger = 1;
 		}
-		
+
 		if (trigger)
 		{
 			if (str[i] > 96 && str[i] < 123)
