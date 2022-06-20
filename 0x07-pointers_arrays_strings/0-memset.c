2b0f54074020bcee3457c119a0433e5716f7e29b
harunazakaria <Harunzy55@gmail.com>
2022-06-20 13:35:48 -0700

    mem

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,13 +6,14 @@
  * @b: constant byte
  * @n: number of bytes of memory area to fill
  * Description: Fill the first `n` bytes of the memory area pointed to by `s`
- * with the constant byte `b`.
+i * with the constant byte `b`.
  * Return: Pointer to memory area `s`
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count = 0;
+
 	while (count < n)
 	{
 		s[count] = b;
